89b652f4cf1603235b29a674736708e83f54b8d8
Vaibhaw0066 <kumar.profile.vaibhaw@gmail.com>
2021-06-19 06:57:43 +0530

    Sending an Array of Struct

--- JAVA Practice/jnaBasics/jna2/src/source-C/struct_modification.c
+++ JAVA Practice/jnaBasics/jna2/src/source-C/struct_modification.c
@@ -1,14 +1,34 @@
 
 
-typedef struct  Modify_struct_in_C{
 
-    int data;
 
-} Modify_struct_in_C;
 
-void modify(Modify_struct_in_C* d){
+// Struct 1
+typedef struct  Struct{
 
-    d->data = 1000;
+    int d;
+
+} Struct;
+
+
+
+
+// ==================================================
+
+// Modify value of Struct
+void modify(Struct* d){
+
+    d->d = 1000;
 
 }
 
+// Receive an array of Structt from JAVA
+int send_struct_array(const Struct* s, int n){
+
+    int total = 0;
+    for(int i=0;i<n;i++){
+
+        total+= s[i].d;
+    } 
+    return total;
+}
---
+++ JAVA Practice/jnaBasics/jna2/src/struct/Send_array_of_struct.java
@@ -0,0 +1,56 @@
+package struct;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Structure;
+import com.sun.jna.Structure.FieldOrder;
+
+import struct.Send_array_of_struct.Struct.ByReference;
+
+public class Send_array_of_struct {
+
+	@FieldOrder({"val"})
+	public static class Struct extends Structure{
+		public static class ByReference extends Struct implements Structure.ByReference{}
+		
+		public int val;
+		
+	}
+	
+	public interface Clib extends Library{
+		
+	
+		Clib INSTANCE = (Clib)Native.load("/home/vaibhaw/Desktop/ZOHO_internship/JAVA Practice/jnaBasics/jna2/src/sharedLib/modify_struct.so",Clib.class);
+		
+		public int send_struct_array(Struct.ByReference Struct, int size);
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		// Struct pointer Refferece
+		Struct.ByReference st = new Struct.ByReference();
+		
+		// assigning array to struct reference pointer
+		Struct[] arr = (Struct[])st.toArray(4);
+		
+		// Assigning values
+		arr[0].val=10;
+		arr[1].val=20;
+
+		arr[2].val=30;
+		arr[3].val=40;
+		
+		int total = Clib.INSTANCE.send_struct_array(st, 4);
+		System.out.println("Total : " + total );
+		
+		
+		
+		
+
+	}
+
+}
